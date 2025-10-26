72bbd8e3c0adf17ca155db75a9d7377d8ce4b203
AlanWaken1 <alanxcl2309@gmail.com>
2025-10-25 22:20:59 -0600

    arreglado: manifest.json2

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,17 @@
 import { updateSession } from "@/lib/supabase/middleware";
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request);
+    const url = request.nextUrl.pathname;
+
+    if (
+        url.startsWith("/api/manifest") ||
+        url.startsWith("/sw.js") ||
+        url.startsWith("/icons/")
+    ) {
+        return NextResponse.next();
+    }
+    return await updateSession(request);
 }
 
 export const config = {
